9cc52da55e49fa1c16978c019679fd575d381ca4
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-06 13:42:04 +0700

    multiple endpoint with ServeMux

--- server_test.go
+++ server_test.go
@@ -6,6 +6,37 @@ import (
 	"testing"
 )
 
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Hello, ServeMux")
+	})
+
+	mux.HandleFunc("/about", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "About Page")
+	})
+
+	mux.HandleFunc("/profile", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Profile Page")
+	})
+
+	mux.HandleFunc("/profile/detail", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Detail Profile Page")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8001",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestHandler(t *testing.T) {
 	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, Server")
