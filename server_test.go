7f5cff6ac81fdc3ce40a03f947f0faf8d7d4ae7c
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-06 13:29:00 +0700

    run custom HTTP handler

--- server_test.go
+++ server_test.go
@@ -1,9 +1,27 @@
 package golang_web
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, Server")
+	}
+
+	server := http.Server{
+		Addr:    "localhost:8001",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
 
 func TestServer(t *testing.T) {
 	server := http.Server{
@@ -15,5 +33,4 @@ func TestServer(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-
 }
