0daee5a7e4864fc2dd14454ef797cd861cb9d43e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-16 20:57:53 +0700

    handling query parameter in go

--- http_test.go
+++ http_test.go
@@ -12,6 +12,29 @@ func HelloHandler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello Go")
 }
 
+func SaySalam(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+
+	if name == "" {
+		fmt.Fprint(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Hello, %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8000?name=jamal", nil)
+
+	recorder := httptest.NewRecorder()
+
+	SaySalam(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
 func TestHttp(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8000/hello", nil)
 	recorder := httptest.NewRecorder()
