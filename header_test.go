1c83d5c3efddb44b5482570158b79e0f8425effa
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-16 21:52:28 +0700

    handling response header

--- header_test.go
+++ header_test.go
@@ -8,6 +8,27 @@ import (
 	"testing"
 )
 
+func ResponseHeader(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("X-Powered-By", "Nasi Cumi Hitam Pak Kris")
+
+	fmt.Fprint(writer, "OK")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8000", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+
+	fmt.Println(response.Header.Get("x-powered-by"))
+}
+
 func RequestHeader(writer http.ResponseWriter, request *http.Request) {
 	contentType := request.Header.Get("content-type")
 
