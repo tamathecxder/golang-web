b5f413fb762545ac16f7e5a1ba267e7b1afe0aae
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-19 15:19:38 +0700

    custom serve file with embed in go

--- serve_file_test.go
+++ serve_file_test.go
@@ -1,6 +1,8 @@
 package golang_web
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -13,10 +15,36 @@ func ServeFile(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+//go:embed resources/dashboard.html
+var resourceDashboard string
+
+//go:embed resources/401.html
+var resourceUnauthorized string
+
+func ServeFileEmbed(writer http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("auth_key") != "" {
+		fmt.Fprint(writer, resourceDashboard)
+	} else {
+		fmt.Fprint(writer, resourceUnauthorized)
+	}
+}
+
 func TestServeFileServer(t *testing.T) {
 	server := http.Server{
 		Addr:    "localhost:8080",
 		Handler: http.HandlerFunc(ServeFile),
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		panic(err)
+	}
+}
+func TestServeFileEmbedServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
 	}
 
 	err := server.ListenAndServe()
