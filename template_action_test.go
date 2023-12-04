430c4d71bf08e573cc27a1567246d22ed7023cc5
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-12-04 20:24:42 +0700

    implement previous gohtml to do comparison action

--- template_action_test.go
+++ template_action_test.go
@@ -17,11 +17,33 @@ func IfTemplateAction(writer http.ResponseWriter, request *http.Request) {
 	})
 }
 
-func TestTemplateAction(t *testing.T) {
+func TestTemplateActionIf(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost/", nil)
 	recorder := httptest.NewRecorder()
 
 	IfTemplateAction(recorder, request)
+
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
+
+func ComparisonTemplateAction(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/comparison.gohtml"))
+
+	t.ExecuteTemplate(writer, "comparison.gohtml", map[string]interface{}{
+		"Title": "Comparison Operator",
+		"Age":   "50",
+	})
+}
+
+func TestTemplateActionComparison(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost/", nil)
+	recorder := httptest.NewRecorder()
+
+	ComparisonTemplateAction(recorder, request)
 
 	response := recorder.Result()
 
